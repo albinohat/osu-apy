d9c7334a9b6f2460291c75213575567546d9df88
Albinohat <albinohat@gmail.com>
2014-12-18 23:40:59 -0500

    Moved a print statement to its own line.

--- osu_player_stats.py
+++ osu_player_stats.py
@@ -11,7 +11,7 @@ import osu_apy
 
 if (len(sys.argv) != 3):
 	print "    Invalid Syntax. Usage: osu_player_stats.py key_file output_file" 
-	print "key_file must contain your osu!api developer key."
+	print "    key_file must contain your osu!api developer key."
 	sys.exit()
 
 f = open(sys.argv[1], "r")
@@ -24,7 +24,7 @@ while(1):
 
 	## Open the file displayed on stream, write to it and close it.
 	stats_file = open(sys.argv[2], "w+")
-	stats_file.write(str(stats_json[0]["username"]).title() + "\nPP: " + str(stats_json[0]["pp_raw"]).split(".", 1)[0] + "\nAccuracy: " + str(stats_json[0]["accuracy"])[:5] + "%")
+	stats_file.write(str(stats_json[0]["username"]).title() + "\nRank:" + str(stats_json[0]["pp_rank"]) + "\nPP: " + str(stats_json[0]["pp_raw"]).split(".", 1)[0] + "\nAccuracy: " + str(stats_json[0]["accuracy"])[:5] + "%")
 	stats_file.close()
 	
 	## Update once per minute.
